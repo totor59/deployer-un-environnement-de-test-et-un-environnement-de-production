3fc9270392794b17dc5fd207b19f7c16c03fcce1
t0t0r <victormarechal59@gmail.com>
2023-08-29 10:25:56 +0200

    🚧 azure_container_registry_block

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-data "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "main" {
   name = "${var.prefix}-rg-${var.suffix}"
+}
+
+data "azurerm_container_registry" "main" {
+  name                = var.prefix
+  resource_group_name = data.azurerm_resource_group.main.name
 }
